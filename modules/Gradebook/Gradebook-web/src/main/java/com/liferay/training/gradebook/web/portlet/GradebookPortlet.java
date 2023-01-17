3e6d40921e5fc7f90836647b8b58db3df59408dd
AntonPodlatov <anton4792@tutanota.com>
2023-01-17 14:46:47 +0700

    Portlet name constant updated - used fully qualified name of the portlet class

--- modules/Gradebook/Gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
+++ modules/Gradebook/Gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -19,7 +19,7 @@ import org.osgi.service.component.annotations.Component;
                 "javax.portlet.display-name=Gradebook",
                 "javax.portlet.init-param.template-path=/",
                 "javax.portlet.init-param.view-template=/view.jsp",
-                "javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+                "javax.portlet.name=" + GradebookPortletKeys.Gradebook,
                 "javax.portlet.resource-bundle=content.Language",
                 "javax.portlet.security-role-ref=power-user,user",
 
