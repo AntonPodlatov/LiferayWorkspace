ee246d7a16fb80113b13a56201ecacb2c8bdf484
AntonPodlatov <anton4792@tutanota.com>
2023-01-17 12:41:10 +0700

    added ProviderType dependency in build.gradle

--- modules/Gradebook/Gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentLocalServiceImpl.java
+++ modules/Gradebook/Gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentLocalServiceImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -21,10 +21,8 @@ import org.osgi.service.component.annotations.Component;
 
 /**
  * The implementation of the assignment local service.
- *
  * <p>
  * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.training.gradebook.service.AssignmentLocalService</code> interface.
- *
  * <p>
  * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
  * </p>
@@ -33,14 +31,12 @@ import org.osgi.service.component.annotations.Component;
  * @see AssignmentLocalServiceBaseImpl
  */
 @Component(
-	property = "model.class.name=com.liferay.training.gradebook.model.Assignment",
-	service = AopService.class
-)
+        property = "model.class.name=com.liferay.training.gradebook.model.Assignment",
+        service = AopService.class)
 public class AssignmentLocalServiceImpl extends AssignmentLocalServiceBaseImpl {
+    /*
+     * NOTE FOR DEVELOPERS:
+     * Never reference this class directly. Use <code>com.liferay.training.gradebook.service.AssignmentLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.training.gradebook.service.AssignmentLocalServiceUtil</code>.
+     */
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.training.gradebook.service.AssignmentLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.training.gradebook.service.AssignmentLocalServiceUtil</code>.
-	 */
 }
