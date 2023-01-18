6fef0cabcc2b4c8edb514718f359132c9aa6a23b
AntonPodlatov <anton4792@tutanota.com>
2023-01-18 12:46:03 +0700

    added: success and error messages for the ui

--- modules/Gradebook/Gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/action/DeleteAssignmentMVCActionCommand.java
+++ modules/Gradebook/Gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/action/DeleteAssignmentMVCActionCommand.java
@@ -3,6 +3,8 @@ package com.liferay.training.gradebook.web.portlet.action;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.training.gradebook.service.AssignmentService;
 import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
@@ -25,17 +27,22 @@ import javax.portlet.ActionResponse;
                 "mvc.command.name=" + MVCCommandNames.DELETE_ASSIGNMENT
         },
         service = MVCActionCommand.class)
-
 public class DeleteAssignmentMVCActionCommand extends BaseMVCActionCommand {
     @Override
-    protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+    protected void doProcessAction(
+            ActionRequest actionRequest,
+            ActionResponse actionResponse
+    ) throws Exception {
         //Get assignment id from request.
         long assignmentId = ParamUtil.getLong(actionRequest, "assignmentId");
         try {
-            //Call service to delete the assignment.
+            // Call service to delete the assignment.
             _assignmentService.deleteAssignment(assignmentId);
+            // Set success message.
+            SessionMessages.add(actionRequest, "assignmentDeleted");
         } catch (PortalException pe) {
-            pe.printStackTrace();
+            // Set error messages from the service layer.
+            SessionErrors.add(actionRequest, "serviceErrorDetails", pe);
         }
     }
 
